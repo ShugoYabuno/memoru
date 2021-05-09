548e934f534ae24ce97c5b5ee54c228de1a066b7
ShugoYabuno <shugo.ybn@gmai.com>
2021-05-09 15:20:39 +0900

    タブ表示

--- lib/edit.dart
+++ lib/edit.dart
@@ -1,19 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+// import 'package:hooks_riverpod/hooks_riverpod.dart';
+// import 'package:flutter_hooks/flutter_hooks.dart';
 
-class Edit extends StatefulWidget {
-  @override
-  _EditState createState() => new _EditState();
-}
+class Edit extends HookWidget {
+  const Edit({Key? key}) : super(key: key);
 
-class _EditState extends State<Edit> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("編集"),
-        ),
-        body: Center(
-          child: Text("編集画面です"),
-        ));
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 2,
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text('TabBar Widget'),
+            bottom: const TabBar(
+              tabs: <Widget>[
+                Tab(
+                  icon: Icon(Icons.cloud_outlined),
+                ),
+                Tab(
+                  icon: Icon(Icons.beach_access_sharp),
+                ),
+              ],
+            ),
+          ),
+          body: const TabBarView(children: <Widget>[
+            Center(
+              child: Text('It\'s cloudy here'),
+            ),
+            Center(
+              child: Text('It\'s rainy here'),
+            ),
+          ])),
+    );
   }
 }
